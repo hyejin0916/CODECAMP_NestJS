fe08fab23b61600d51e2f641fa88d54c741ebcea
Kanghyejin <rkdgpwlsz@naver.com>
2023-05-08 16:14:12 +0900

    localhost 이슈 수정

--- section08/08-05-nestjs-with-typeorm-api/src/app.module.ts
+++ section08/08-05-nestjs-with-typeorm-api/src/app.module.ts
@@ -17,7 +17,7 @@ import { Board } from './apis/boards/entities/board.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'gpwls6240',
